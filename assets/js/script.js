d577dd8cf52e721b016e90cd8eb80b1ef8e09eb8
Thomas Stemler <stemlertho@gmail.com>
2024-08-05 20:55:44 -0500

    First part of javascript finished, movie submission is done mostly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log("Test test test");
+const movieListEl = document.querySelector(".movie-list");
+const movieSuggestionForm = document.querySelector(".suggest-movie");
+
+function movieListRefresh() {
+    
+}
+
+function init() {
+
+}
+const suggestMovie = function (event) {
+    event.preventDefault();
+    const movieArry = JSON.parse(localStorage.getItem("MovieArray")) || [];
+
+    const movieTitle = movieSuggestionForm.querySelector("#movieName").value;
+    if (movieTitle == "") 
+        return;
+    const movie = {
+        points: 0,
+        votes: 0,
+        title: movieTitle
+    };
+    movieArry.push(movie);
+    localStorage.setItem("MovieArray", JSON.stringify(movieArry));
+    console.log(movieArry);
+}
+
+movieSuggestionForm.querySelector("#suggest").addEventListener("click", suggestMovie);
